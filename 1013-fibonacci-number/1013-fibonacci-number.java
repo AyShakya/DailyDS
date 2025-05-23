9fd76c9816fbdc9fea1d71b8c1618cd570fd65cd
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-05-23 14:58:25 +0530

    Time: 77 ms (10.67%), Space: 44.6 MB (6.76%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,18 +1,15 @@
 class Solution {
-    private int fibo(int n){
+    public int fib(int n) {
         int DAT[] = new int[40];
         if(n<2){
             return n;
         }
         else{
             if(DAT[n]==0){
-                DAT[n]=fibo(n-1)+fibo(n-2);
+                DAT[n]=fib(n-1)+fib(n-2);
                 return DAT[n];
             }
         }
         return 0;
-    }
-    public int fib(int n) {
-        return fibo(n);
     }
 }
