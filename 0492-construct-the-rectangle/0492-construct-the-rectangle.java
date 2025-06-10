90381e9d8c768c35aa75139c5134e8a6bbc2090a
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-10 22:42:39 +0530

    Time: 222 ms (5.41%), Space: 40.8 MB (83.94%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -1,8 +1,13 @@
 class Solution {
     public int[] constructRectangle(int area) {
-        int minlen = Integer.MAX_VALUE, L=0, W=0;
         int A[] = new int[2];
-        for(int i=1;i<=area;i++){
+        if(area==1){
+            A[0]=1;
+            A[1]=1;
+            return A;
+        }
+        int minlen = Integer.MAX_VALUE, L=0, W=0;
+        for(int i=1;i<=area/2;i++){
             if(area%i==0){
                 int a=area/i;
                 int b=i;
