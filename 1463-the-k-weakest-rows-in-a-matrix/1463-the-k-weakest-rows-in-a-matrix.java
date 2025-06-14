eb740a1574448b7972435124c470c1b428a6b3a1
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-14 20:45:11 +0530

    Time: 5 ms (7.19%), Space: 45.5 MB (6.26%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -11,7 +11,12 @@ class Solution {
         for(int rowIndex = 0; rowIndex < mat.length; rowIndex++){
             int sum=0;
             for(int val : mat[rowIndex]){
-                sum+=val;
+                if(val==1){
+                    sum+=val;
+                }
+                else{
+                    break;
+                }
             }
             cal.add(new Pair(sum, rowIndex));
         }
