9819dd7f890b9e74e8841cbb521556cf2fa42007
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-14 20:24:59 +0530

    Time: 25 ms (28.53%), Space: 46.3 MB (18.69%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.java
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -1,43 +1,49 @@
 class Solution {
-    private boolean binarySeach(int[] arr, int target){
-        int left=0, right=arr.length-1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(arr[mid]==target){
-                return true;
-            }
-            else{
-                if(arr[mid]>target){
-                    right=mid-1;
-                }
-                else{
-                    left=mid+1;
-                }
-            }
-        }
-        return false;
-    }
+    // private boolean binarySeach(int[] arr, int target){
+    //     int left=0, right=arr.length-1;
+    //     while(left<=right){
+    //         int mid = (left+right)/2;
+    //         if(arr[mid]==target){
+    //             return true;
+    //         }
+    //         else{
+    //             if(arr[mid]>target){
+    //                 right=mid-1;
+    //             }
+    //             else{
+    //                 left=mid+1;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
     public int[] fairCandySwap(int[] a, int[] b) {
         Arrays.sort(a);
         Arrays.sort(b);
         int alice=0, bob=0;
-        for(int i=0;i<a.length;i++){
-            alice+=a[i];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : a){
+            alice+=i;
         }
-        for(int i=0;i<b.length;i++){
-            bob+=b[i];
+        for(int i : b){
+            bob+=i;
+            map.put(i, map.getOrDefault(i, 0)+1);
         }
-        int diff = alice>bob?alice:bob;
-        int[] res = new int[2];
-        for(int k=1;k<=diff;k++){
-            int i = k;
-            int j = (bob-alice+2*i)/2;
-            if(binarySeach(a,i) && binarySeach(b,j)){
-                res[0]=i;
-                res[1]=j;
-                break;
+        // for(int k=1;k<=diff;k++){
+        //     int i = k;
+        //     int j = (bob-alice+2*i)/2;
+        //     if(binarySeach(a,i) && binarySeach(b,j)){
+        //         res[0]=i;
+        //         res[1]=j;
+        //         break;
+        //     }
+        // }
+        for(int i : a){
+            int j = (bob - alice +2*i)/2;
+            if(map.containsKey(j)){
+                return new int[] {i,j};
             }
         }
-        return res;
+        return new int[] {0,0};
     }
 }
