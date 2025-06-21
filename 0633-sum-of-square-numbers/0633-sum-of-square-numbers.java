5f0488f6cb9c312ff34805b14994083df3dec323
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-22 02:27:29 +0530

    Time: 3 ms (95.06%), Space: 40.5 MB (62.38%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,32 +1,16 @@
 class Solution {
-    private boolean Psquare(int num) {
-        if(num==1) return true;
-        long left = 0, right = num/2;
-        while(left<=right){
-            long mid = (left+right)/2;
-            long sqr = mid*mid;
-            if(sqr==num){
-                return true;
-            }
-            else if(sqr<num){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
-        }
-        return false;
-    }
     public boolean judgeSquareSum(int c) {
-        if(c==0 || c==1){
-            return true;
+        long left = 0;
+        long right = (long) Math.sqrt(c);
+
+        while (left <= right) {
+            long sum = left * left + right * right;
+
+            if (sum == c) return true;
+            else if (sum < c) left++;
+            else right--;
         }
-        for(int i=1;i<=Math.sqrt(c);i++){
-            int k = c-i*i;
-            if(Psquare(k)){
-                return true;
-            }
-        }
+
         return false;
     }
 }
