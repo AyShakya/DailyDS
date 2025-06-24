41c633103c71c3ba30cb66f437c17ca06b54d20a
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-24 13:23:18 +0530

    Time: 13 ms (88.49%), Space: 43.5 MB (35.17%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,13 +1,16 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int res=0, minlen = Integer.MAX_VALUE;
         Arrays.sort(nums);
         for(int k=0;k<nums.length-2;k++){
             if (k > 0 && nums[k] == nums[k - 1]) continue;
             int i=k+1,j=nums.length-1;
             while(i<j){
                 int sum=nums[k]+nums[i]+nums[j];
-                map.put(sum, 1);
+                if(Math.abs(sum-target)<minlen){
+                    minlen=Math.abs(sum-target);
+                    res=sum;
+                }
                 if(sum==target){
                     return sum;
                 }
@@ -17,13 +20,6 @@ class Solution {
                 else{
                     i++;
                 }
-            }
-        }
-        int res=0, minlen = Integer.MAX_VALUE;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(Math.abs(entry.getKey()-target)<minlen){
-                minlen=Math.abs(entry.getKey()-target);
-                res=entry.getKey();
             }
         }
         return res;
