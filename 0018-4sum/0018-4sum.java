054954aa501b98feba01c9095a6cfdbfafb5000f
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-06-24 13:59:44 +0530

    Time: 21 ms (19.74%), Space: 44.3 MB (27.11%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,7 +3,9 @@ class Solution {
         Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
         for (int p = 0; p < nums.length - 3; p++) {
+            if(p>0 && nums[p-1]==nums[p]) continue;
             for (int k = p + 1; k < nums.length - 2; k++) {
+                if(k!=p+1 && nums[k]==nums[k-1]) continue;
                 int i = k + 1, j = nums.length - 1;
                 while (i < j) {
                     long sum = (long) nums[k] + nums[i] + nums[j] + nums[p];
@@ -11,6 +13,8 @@ class Solution {
                         result.add(Arrays.asList(nums[p], nums[k], nums[i], nums[j]));
                         i++;
                         j--;
+                        while(i<j && nums[i]==nums[i-1]) i++;
+                        while(i<j && nums[j]==nums[j+1]) j--;
                     } else if (sum > (long)target) {
                         j--;
                     } else {
