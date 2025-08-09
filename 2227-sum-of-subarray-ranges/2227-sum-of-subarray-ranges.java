d92d2ab3c613b081d280d67b6ad911cc6569f8ab
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-08-09 21:04:13 +0530

    Time: 20 ms (54.27%), Space: 44.3 MB (80.75%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,37 +1,32 @@
 class Solution {
-    public long sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        Stack<Integer> s = new Stack<>();
-        long sum = 0;
-        for (int i = 0; i <= n; i++) {
-            int curr = (i == n) ? Integer.MIN_VALUE : arr[i]; 
-            while (!s.isEmpty() && curr < arr[s.peek()]) {
-                int mid = s.pop();
-                int left = s.isEmpty() ? mid + 1 : mid - s.peek();
-                int right = i - mid;
-                sum = (sum + (long) arr[mid] * left * right);
-            }
-            s.push(i);
-        }
-        return sum;
-    }
-    public long sumSubarrayMaxs(int[] arr) {
-        int n = arr.length;
-        Stack<Integer> s = new Stack<>();
-        long sum = 0;
-        for (int i = 0; i <= n; i++) {
-            int curr = (i == n) ? Integer.MAX_VALUE : arr[i]; 
-            while (!s.isEmpty() && curr > arr[s.peek()]) {
-                int mid = s.pop();
-                int left = s.isEmpty() ? mid + 1 : mid - s.peek();
-                int right = i - mid;
-                sum = (sum + (long) arr[mid] * left * right);
-            }
-            s.push(i);
-        }
-        return sum;
-    }
     public long subArrayRanges(int[] nums) {
-        return (sumSubarrayMaxs(nums)-sumSubarrayMins(nums));
+        int n = nums.length;
+        long minSum = 0, maxSum = 0;
+
+        Stack<Integer> minStack = new Stack<>();
+        Stack<Integer> maxStack = new Stack<>();
+
+        for (int i = 0; i <= n; i++) {
+            int currMin = (i == n) ? Integer.MIN_VALUE : nums[i];
+            int currMax = (i == n) ? Integer.MAX_VALUE : nums[i];
+
+            while (!minStack.isEmpty() && currMin < nums[minStack.peek()]) {
+                int mid = minStack.pop();
+                int left = minStack.isEmpty() ? mid + 1 : mid - minStack.peek();
+                int right = i - mid;
+                minSum += (long) nums[mid] * left * right;
+            }
+            minStack.push(i);
+
+            while (!maxStack.isEmpty() && currMax > nums[maxStack.peek()]) {
+                int mid = maxStack.pop();
+                int left = maxStack.isEmpty() ? mid + 1 : mid - maxStack.peek();
+                int right = i - mid;
+                maxSum += (long) nums[mid] * left * right;
+            }
+            maxStack.push(i);
+        }
+
+        return maxSum - minSum;
     }
 }
