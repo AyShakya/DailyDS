df6406c80791b10c2797ed3b33e37364cb8e0278
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-08-10 18:52:15 +0530

    Time: 21 ms (94.64%), Space: 57.3 MB (80.09%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,23 +1,26 @@
 class Solution {
     public int largestRectangleArea(int[] nums) {
-        Stack<Integer> s = new Stack<>();
-        int maxArea = 0;
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            while(!s.isEmpty() && nums[i]<nums[s.peek()]){
-                int element = s.pop();
-                int nse = i;
-                int pse = s.isEmpty() ? -1 : s.peek();
-                maxArea = Math.max(maxArea, (nums[element]*((nse-pse)-1)));
-            }
-            s.push(i);
+    Deque<Integer> s = new ArrayDeque<>();
+    int maxArea = 0;
+    int n = nums.length;
+
+    for (int i = 0; i < n; i++) {
+        while (!s.isEmpty() && nums[i] < nums[s.peekLast()]) {
+            int element = s.removeLast();
+            int nse = i;
+            int pse = s.isEmpty() ? -1 : s.peekLast();
+            maxArea = Math.max(maxArea, nums[element] * ((nse - pse) - 1));
         }
-        while(!s.isEmpty()){
-            int element = s.pop();
-            int nse = n;
-            int pse = s.isEmpty() ? -1 : s.peek();
-            maxArea = Math.max(maxArea, (nums[element]*((nse-pse)-1)));
-        }
-        return maxArea;
+        s.addLast(i);
     }
+
+    while (!s.isEmpty()) {
+        int element = s.removeLast();
+        int nse = n;
+        int pse = s.isEmpty() ? -1 : s.peekLast();
+        maxArea = Math.max(maxArea, nums[element] * ((nse - pse) - 1));
+    }
+
+    return maxArea;
+}
 }
