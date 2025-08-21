02f2872fe00895c6e2b47a43add5596efcbc4212
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-08-21 20:09:37 +0530

    Time: 123 ms (5.78%), Space: 41.7 MB (79.74%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -5,7 +5,7 @@ class Solution {
 
         StringBuilder sb = new StringBuilder(word.substring(0,count+1));
         sb.reverse();
-        sb.append(word.substring(count+1,word.length()));
+        sb.append(word.substring(count+1));
         return sb.toString();
     }
 }
