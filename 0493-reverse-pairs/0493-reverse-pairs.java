dc36ebfe61a4d6124b31e5d2ac279a7907ca22cc
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-09-11 21:20:58 +0530

    Time: 45 ms (53.49%), Space: 55.6 MB (13.8%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,58 @@
+class Solution {
+    public void merge(int arr[], int l, int m, int r){
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public int countPairs(int[] nums, int low, int mid, int high){
+        int cnt = 0;
+        int right = mid+1;
+        for(int i = low;i<=mid;i++){
+            while(right<=high && (long)nums[i]>(long)2*nums[right]) right++;
+            cnt+= (right - (mid+1));
+        }
+        return cnt;
+    }
+    public int mergeSort(int arr[], int l, int r){ 
+        int cnt = 0;
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            cnt += mergeSort(arr, l, m);
+            cnt += mergeSort(arr, m + 1, r);
+            cnt += countPairs(arr, l, m, r);
+            merge(arr, l, m, r);
+        }
+        return cnt;
+    }
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length-1);
+    }
+}
