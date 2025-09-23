76f20ad90f8a82847d14f9d721ac635f66146c20
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-09-23 13:15:50 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,36 +2,13 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-        int i=0, j=0;
-        int n = v1.length;
-        int m = v2.length;
-        while(i<n && j<m){
-            int k1 = Integer.parseInt(v1[i]);
-            int k2 = Integer.parseInt(v2[j]);
-            if(k1==k2){
-                i++;
-                j++;
-            }
-            else if(k1>k2){
-                return 1;
-            }
-            else if(k1<k2){
-                return -1;
-            }
-        }
-        while(i<n){
-            int k1 = Integer.parseInt(v1[i]);
-            if(k1!=0){
-                return 1;
-            }
-            i++;
-        }
-        while(j<m){
-            int k2 = Integer.parseInt(v2[j]);
-            if(k2!=0){
-                return -1;
-            }
-            j++;
+        int n = Math.max(v1.length,v2.length);
+        for(int i=0;i<n;i++){
+            int k1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int k2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+
+            if(k1<k2) return -1;
+            if(k1>k2) return 1;
         }
         return 0;
     }
