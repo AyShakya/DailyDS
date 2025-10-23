3f33fa0bc0e5ccbd8e52363b41bbac0e7405bf6b
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-10-23 15:54:22 +0530

    Time: 8 ms (72.03%), Space: 44.7 MB (69.77%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -3,8 +3,7 @@ class Solution {
         while(s.length()>2){
             StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length()-1;i++){
-            int sum = ((s.charAt(i)-'0') + (s.charAt(i+1)-'0'))%10;
-            sb.append(String.valueOf(sum));
+            sb.append(((s.charAt(i)-'0') + (s.charAt(i+1)-'0'))%10);
         }
         s=sb.toString();
         }
