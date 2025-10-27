5cd44463da38602d9111286d2968cc719bd7d830
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-10-27 14:28:51 +0530

    Time: 10 ms (89.6%), Space: 45 MB (76.08%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -1,17 +1,19 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        List<Integer> ans = new ArrayList<>();
+        int prev = 0;
+        int res = 0;
+        int c = 0;
         for(String s : bank){
-            int c = 0;
+            if(c!=0){
+                prev = c;
+            }
+            c=0;
             for(char ch : s.toCharArray()){
                 if(ch=='1') c++;
             }
-            if(c!=0) ans.add(c);
-        }
-        int res = 0;
-        int n = ans.size();
-        for(int i=0;i<n-1;i++){
-            res+= ans.get(i)*ans.get(i+1);
+            if(c!=0){
+                res+=prev*c; 
+            };
         }
         return res;
     }
