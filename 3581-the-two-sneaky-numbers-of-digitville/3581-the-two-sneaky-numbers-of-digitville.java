4823839471eda71c475665ca4cc0274acd79859a
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-10-31 09:48:36 +0530

    Time: 2 ms (63.09%), Space: 44.8 MB (59.98%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int[] ans = new int[2];
+        Boolean flag = true;
+        for(int i : nums){
+            if(set.add(i)){
+                continue;
+            }
+            else{
+                if(flag){
+                    flag = false;
+                    ans[0] = i;
+                }
+                else{
+                    ans[1] = i;
+                }
+            }
+        }
+        return ans;
+    }
+}
