c0ce14893ee337a2bd8103a8129f22de1fc8c620
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-11-21 22:43:36 +0530

    Time: 34 ms (98.18%), Space: 46.9 MB (28.57%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int res = 0;
+        int[] first = new int[26];
+        int[] last = new int[26];
+
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
+
+        for(int i = 0; i < n; i++){
+            int index = s.charAt(i) - 'a';
+            if(first[index] == -1){
+                first[index] = i;
+            }
+            last[index] = i;
+        }
+
+        for(int i=0;i<26;i++){
+            if(first[i]!=-1 && first[i]<last[i]){
+                boolean[] seen = new boolean[26];
+                for(int j = first[i]+1; j<last[i];j++){
+                    int ind = s.charAt(j) - 'a';
+                    if(!seen[ind]){
+                        seen[ind] = true;
+                        res++;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
