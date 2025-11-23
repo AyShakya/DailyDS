4090a0515270ab804c2a9e5642efd2d90befb033
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-11-23 11:05:02 +0530

    Time: 5 ms (89.75%), Space: 49.4 MB (47.52%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,44 +1,28 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-        
-        ArrayList<Integer> r1 = new ArrayList<>();
-        ArrayList<Integer> r2 = new ArrayList<>();
         int sum = 0;
-        for(int i : nums){
-            if(i%3==1) r1.add(i);
-            if(i%3==2) r2.add(i);
-            sum+=i;
+        int oneRem = 20000;
+        int twoRem = 20000;
+        for(int num : nums){
+            sum += num;
+            if(num%3==1){
+                twoRem = Math.min(twoRem, oneRem+num);
+                oneRem = Math.min(oneRem, num);
+            }
+            if(num%3==2){
+                oneRem = Math.min(oneRem, twoRem+num);
+                twoRem = Math.min(twoRem, num);
+            }
         }
-        Collections.sort(r1);
-        Collections.sort(r2);
+        if(sum%3==0){
+            return sum;
+        }
         if(sum%3==1){
-            if(r2.size()>=2 && r1.size()>=1){
-                sum = sum - Math.min(r1.get(0), r2.get(0)+r2.get(1));
-            }
-            else if(r1.size()>=1){
-                sum -= r1.get(0);
-            }
-            else if(r2.size()>=2){
-                sum -= r2.get(0)+r2.get(1);
-            }
-            else{
-                sum = 0;
-            }
+            return sum - oneRem;
         }
-        else if(sum%3==2){
-            if(r2.size()>=1 && r1.size()>=2){
-                sum = sum - Math.min(r2.get(0), r1.get(0)+r1.get(1));
-            }
-            else if(r1.size()>=2){
-                sum -= r1.get(0) + r1.get(1);
-            }
-            else if(r2.size()>=1){
-                sum -= r2.get(0);
-            }
-            else{
-                sum = 0;
-            }
+        if(sum%3==2){
+            return sum - twoRem;
         }
-        return sum;
+        return 0;
     }
 }
