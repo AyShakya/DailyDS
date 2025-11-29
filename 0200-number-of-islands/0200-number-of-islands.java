283af34dce21107845a6243a8e4b5ca4732c098c
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-11-29 15:53:38 +0530

    Time: 4 ms (49.03%), Space: 51.9 MB (46.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,49 +1,25 @@
-class Pair {
-    int first, second;
-
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
     public int numIslands(char[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] status = new int[n][m];
-        int count = 0;
-        for (int a = 0; a < n; a++) {
-            for (int b = 0; b < m; b++) {
-                if (grid[a][b] == '1' && status[a][b] == 0) {
-                    count++;
-                    q.offer(new Pair(a, b));
-                    status[a][b] = 2;
-                    while (!q.isEmpty()) {
-                        Pair p = q.poll();
-                        int i = p.first;
-                        int j = p.second;
-                        if (i + 1 < n && grid[i + 1][j] == '1' && status[i + 1][j] != 2) {
-                            q.offer(new Pair(i + 1, j));
-                            status[i + 1][j] = 2;
-                        }
-                        if (i - 1 >= 0 && grid[i - 1][j] == '1' && status[i - 1][j] != 2) {
-                            q.offer(new Pair(i - 1, j));
-                            status[i - 1][j] = 2;
-                        }
-                        if (j + 1 < m && grid[i][j + 1] == '1' && status[i][j + 1] != 2) {
-                            q.offer(new Pair(i, j + 1));
-                            status[i][j + 1] = 2;
-                        }
-                        if (j - 1 >= 0 && grid[i][j - 1] == '1' && status[i][j - 1] != 2) {
-                            q.offer(new Pair(i, j - 1));
-                            status[i][j - 1] = 2;
-                        }
-                    }
+        int c = 0;
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    c++;
+                    dfs(i,j,grid,visited);
                 }
             }
         }
-        return count;
+        return c;
+    }
+    public static void dfs(int r, int c, char[][] grid, boolean[][] visited){
+        if(r<0 || r>=visited.length || c<0 || c>=visited[0].length || visited[r][c] || grid[r][c]=='0'){
+            return;
+        }
+        visited[r][c] = true;
+        dfs(r+1,c,grid,visited);
+        dfs(r,c+1,grid,visited);
+        dfs(r-1,c,grid,visited);
+        dfs(r,c-1,grid,visited);
     }
 }
