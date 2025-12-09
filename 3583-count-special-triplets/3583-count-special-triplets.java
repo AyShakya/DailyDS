18ab85cac77d5b1ec50fd755295e1bc7d9253672
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-12-09 20:35:51 +0530

    Time: 67 ms (89.92%), Space: 276.7 MB (5.43%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.java
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -1,49 +1,30 @@
 class Solution {
-    private int lowerBound(ArrayList<Integer> list, int target) {
-        int lo = 0, hi = list.size(); 
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (list.get(mid) >= target) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return lo;
-    }
-    private int upperBound(ArrayList<Integer> list, int target) {
-        int lo = 0, hi = list.size(); 
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (list.get(mid) > target) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        return lo;
-    }
     public int specialTriplets(int[] nums) {
-        long ans = 0;
-        int mod = 1000000007;
         int n = nums.length;
-        Map<Integer, ArrayList<Integer>> m = new HashMap<>();
-        for(int i=0;i<n;i++){
-            int val = nums[i];
-            m.computeIfAbsent(val, k -> new ArrayList<>()).add(i);
-        }
-        for(int i=0;i<n;i++){
-            long valLong = (long) nums[i] * 2;
-            if (valLong > Integer.MAX_VALUE) continue;
-            int val = (int) valLong;
+        int MOD = 1_000_000_007;
 
-            ArrayList<Integer> list = m.get(val);
-            if (list == null) continue;
+        int MAX = 200_000;
+        int[] left = new int[MAX + 1];
+        int[] right = new int[MAX + 1];
 
-            int c1 = lowerBound(list, i);                
-            int c2 = list.size() - upperBound(list, i);  
-            ans = (ans + (1L * c1 * c2) % mod) % mod;
+        for (int x : nums) {
+            right[x]++;
         }
-        return (int)ans;
+
+        long ans = 0;
+
+        for (int j = 0; j < n; j++) {
+            int val = nums[j];
+            right[val]--;
+
+            int v2 = val * 2;
+            if (v2 <= MAX) {
+                long c1 = left[v2];   
+                long c2 = right[v2];  
+                ans = (ans + (c1 * c2) % MOD) % MOD;
+            }
+            left[val]++;
+        }
+        return (int) ans;
     }
 }
