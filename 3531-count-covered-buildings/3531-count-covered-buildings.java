9e79d936afbeb47ef6a1e53888604cd02b798d29
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-12-11 11:24:20 +0530

    Time: 123 ms (63.16%), Space: 265.6 MB (41.05%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.java
+++ 3531-count-covered-buildings/3531-count-covered-buildings.java
@@ -1,29 +1,32 @@
 class Solution {
+    private static class Info {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+
+        void add(int v) {
+            count++;
+            if (v < min) min = v;
+            if (v > max) max = v;
+        }
+    }
     public int countCoveredBuildings(int n, int[][] buildings) {
-        Map<Integer, ArrayList<Integer>> mx = new HashMap<>();
-        Map<Integer, ArrayList<Integer>> my = new HashMap<>();
+        Map<Integer, Info> mx = new HashMap<>();
+        Map<Integer, Info> my = new HashMap<>();
         for (int[] i : buildings) {
             int x = i[0];
             int y = i[1];
-            mx.computeIfAbsent(x, k -> new ArrayList<>()).add(y);
-            my.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
-        }
-        for (ArrayList<Integer> list : mx.values()) {
-            Collections.sort(list);
-        }
-        for (ArrayList<Integer> list : my.values()) {
-            Collections.sort(list);
+            mx.computeIfAbsent(x, k -> new Info()).add(y);
+            my.computeIfAbsent(y, k -> new Info()).add(x);
         }
         int c = 0;
         for (int i[] : buildings) {
             int x = i[0];
             int y = i[1];
-            ArrayList<Integer> listx = mx.get(x);
-            ArrayList<Integer> listy = my.get(y);
-            int a = listx.size();
-            int b = listy.size();
-            if (a == 1 || listx.get(0) == y || listx.get(a - 1) == y) continue;
-            if (b == 1 || listy.get(0) == x || listy.get(b - 1) == x) continue;
+            Info ix = mx.get(x);
+            Info iy = my.get(y);
+            if (ix.count == 1 || ix.min == y || ix.max == y) continue;
+            if (iy.count == 1 || iy.min == x || iy.max == x) continue;
             c++;
         }
         return c;
