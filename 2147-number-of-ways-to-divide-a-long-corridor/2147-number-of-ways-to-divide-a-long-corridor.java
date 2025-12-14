ca225ee4a462033f044287fbee33295a7cd0ac4f
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2025-12-14 22:48:01 +0530

    Time: 20 ms (92.8%), Space: 47.7 MB (58.4%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -1,31 +1,19 @@
 class Solution {
-    private static final int MOD = 1_000_000_007;
-    private int count(int index, int seats, String corridor, Map<Pair<Integer, Integer>, Integer> cache) {
-        if (index == corridor.length()) {
-            return seats == 2 ? 1 : 0;
-        }
-        if (cache.containsKey(new Pair<>(index, seats))) {
-            return cache.get(new Pair<>(index, seats));
-        }
-        int result = 0;
-        if (seats == 2) {
-            if (corridor.charAt(index) == 'S') {
-                result = count(index + 1, 1, corridor, cache);
-            } else {
-                result = (count(index + 1, 0, corridor, cache) + count(index + 1, 2, corridor, cache)) % MOD;  
-            }
-        } else {
-            if (corridor.charAt(index) == 'S') {
-                result = count(index + 1, seats + 1, corridor, cache);
-            } else {
-                result = count(index + 1, seats, corridor, cache);
-            }
-        }
-        cache.put(new Pair<>(index, seats), result);
-        return result;
-    }
     public int numberOfWays(String corridor) {
-        Map<Pair<Integer, Integer>, Integer> cache = new HashMap<>();
-        return count(0, 0, corridor, cache);
+        final int MOD = 1000_000_007;
+        long result = 1;
+        int prevSeatIndex = 0;
+        int numSeats = 0;
+        for(int i = 0; i < corridor.length(); i++){
+            char c = corridor.charAt(i);
+            if(c == 'S'){
+                numSeats++;
+                if(numSeats > 2 && numSeats % 2 == 1){
+                    result = result * (i - prevSeatIndex)%MOD;
+                }
+                prevSeatIndex = i;
+            }
+        }
+        return numSeats > 1 && numSeats % 2 == 0 ? (int)result : 0;
     }
 }
