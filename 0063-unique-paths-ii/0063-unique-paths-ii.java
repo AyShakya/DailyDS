d4b7f7ec2758220a6e61d31b4c8dbab2fc4be06e
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2026-02-10 19:25:16 +0530

    Time: 1 ms (18.35%), Space: 43.1 MB (98.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,35 +1,42 @@
 class Solution {
+    public int f(int m, int n, int[][] a, int[][] dp){
+        if(n==0 && m==0){
+            return 1;
+        }
+        if(dp[m][n]!=-1) return dp[m][n];
+        int left = 0;
+        int right = 0;
+        if(n-1>=0 && a[m][n-1]!=1){
+            left = f(m, n-1, a, dp);
+        }
+        if(m-1>=0 && a[m-1][n]!=1){
+            right = f(m-1, n, a, dp);
+        }
+        return dp[m][n] = left + right;
+    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
-        int dp[] = new int[n];
-        dp[0] = 1;
+        if(obstacleGrid[m-1][n-1]==1) return 0;
+        int dp[][] = new int[m][n];
+        for(int i = 0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        dp[0][0] = 1;
         for(int i =0 ;i<m;i++){
-            int temp[] = new int[n];
             for(int j=0;j<n;j++){
-                if (obstacleGrid[i][j] == 1) {
-                    temp[j] = 0;
-                    continue;
-                }
-
-                if (i == 0 && j == 0) {
-                    temp[j] = 1;
-                    continue;
-                }
+                if(i==0 && j==0) continue;
                 int left = 0;
                 int right = 0;
-                if(i>0){
-                    left = dp[j];
+                if(i-1>=0 && obstacleGrid[i-1][j]!=1){
+                    left = dp[i-1][j];
                 }
-                if(j>0){
-                    right = temp[j-1];
+                if(j-1>=0 && obstacleGrid[i][j-1]!=1){
+                    right = dp[i][j-1];
                 }
-                temp[j] = left + right;
+                dp[i][j] = left + right;
             }
-            dp = temp;
         }
-        return dp[n-1];
+        return dp[m-1][n-1];
     }
 }
