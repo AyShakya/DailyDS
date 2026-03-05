a90105a5cd9fe4135ecac97a1ba4e2df2fece88c
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2026-03-05 23:30:39 +0530

    Time: 5 ms (53.53%), Space: 43.7 MB (79.17%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -5,22 +5,16 @@ class Solution {
         int n = s.length();
         for(int i=0;i<n;i++){
             if(i%2==0){
-                if(s.charAt(i)=='1') continue;
-                else ans1++;
+                if(s.charAt(i)=='0') ans1++;
             }
             else{
-                if(s.charAt(i)=='0') continue;
-                else ans1++;
+                if(s.charAt(i)=='1') ans1++;
             }
-        }
-        for(int i=0;i<n;i++){
             if(i%2==0){
-                if(s.charAt(i)=='0') continue;
-                else ans2++;
+                if(s.charAt(i)=='1') ans2++;
             }
             else{
-                if(s.charAt(i)=='1') continue;
-                else ans2++;
+                if(s.charAt(i)=='0') ans2++;
             }
         }
         return Math.min(ans1, ans2);
