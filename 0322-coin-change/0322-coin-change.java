353acdd093d0286a8a872a899188b540d294eaae
Ayush Shakya <98823736+AyShakya@users.noreply.github.com>
2026-03-17 09:56:37 +0530

    Time: 19 ms (38.01%), Space: 47 MB (33.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -13,13 +13,24 @@ class Solution {
         }
         return dp[n][k] = Math.min(np, p);
     }
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
+    public int coinChange(int[] arr, int amount) {
+        int n = arr.length;
         int dp[][] = new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<=amount;i++){
+            if(i%arr[0]==0) dp[0][i] = i/arr[0];
+            else dp[0][i] = Integer.MAX_VALUE;
         }
-        int a = f(n-1, amount, coins, dp); 
-        return a==2147483647? -1:a;
+        for(int i = 1; i<n;i++){
+            for(int k=0;k<=amount;k++){
+                int np = 0 + dp[i-1][k];
+                int p = Integer.MAX_VALUE;
+                if(arr[i]<=k){
+                    int res = dp[i][k-arr[i]];
+                    if(res!=Integer.MAX_VALUE) p = 1 + res;
+                }
+                dp[i][k] = Math.min(np, p);
+            }
+        }
+        return dp[n-1][amount]==Integer.MAX_VALUE?-1:dp[n-1][amount];
     }
 }
